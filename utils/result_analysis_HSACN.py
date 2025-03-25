0d690ab4a701db63fe9093efeebf889afe0fe039
SravyaKodati <kodati.sr@northeastern.edu>
2025-03-24 20:51:19 -0400

    code to analyse validation results

---
+++ utils/result_analysis_HSACN.py
@@ -0,0 +1,67 @@
+import yaml
+import torch
+import pandas as pd
+import torch.nn as nn
+import torch.optim as optim
+
+from HSACN import HSACN
+from final_dataset_HSACN import val_df, val_dataloader
+
+
+with open('hsacn.yaml') as f:
+    config = yaml.safe_load(f)
+
+
+num_users= config['m']['num_users']
+num_items= config['m']['num_items']
+word_embedding_dim = config['m']['word_embedding_dim']
+words_per_sentence = config['m']['words_per_sentence']
+sentences_per_review = config['m']['sentences_per_review']   
+user_reviews_per_entity = config['m']['user_reviews_per_entity']    
+item_reviews_per_entity = config['m']['item_reviews_per_entity'] 
+num_heads = config['m']['num_heads']   
+kernel_size = config['m']['kernel_size'] 
+hidden_dim = config['m']['hidden_dim']
+latent_dim = config['m']['latent_dim'] 
+
+
+glove_embeddings = torch.load('required_embeddings.pt').to(torch.float32)
+
+model = HSACN(num_users, num_items, word_embedding_dim, hidden_dim, latent_dim, kernel_size, num_heads, words_per_sentence,
+                        sentences_per_review, user_reviews_per_entity, item_reviews_per_entity, glove_embeddings)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")  # macOS Metal Performance Shaders
+else:
+    device = torch.device("cpu")
+
+
+model = model.to(device)
+optimizer = optim.Adam(model.parameters(), lr=config['t']['learning_rate'])
+
+
+predicted_ratings = []
+true_ratings = []
+user_ids = []
+item_ids = []
+
+
+with torch.no_grad():
+    for user_tower_input, item_tower_input, true_rating, user_id, item_id in val_dataloader:
+        predicted_rating = model(user_id, item_id, user_tower_input, item_tower_input)
+        predicted_ratings.extend(predicted_rating.cpu().numpy().flatten())
+        true_ratings.extend(true_rating.cpu().numpy().flatten())
+        user_ids.extend(user_id.cpu().numpy().flatten())
+        item_ids.extend(item_id.cpu().numpy().flatten())
+
+results_df = pd.DataFrame({
+    'user_id': user_ids,
+    'item_id': item_ids,
+    'true_rating': true_ratings,
+    'predicted_rating': predicted_ratings
+})
+
+
+merged_df = pd.merge(val_df.reset_index(drop=True), results_df, on=['user_id', 'item_id'])
